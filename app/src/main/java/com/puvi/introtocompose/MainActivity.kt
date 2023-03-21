cffa76ddf06b88e6cd109188b2dfb148c8629367
Puviarasan <vc.puviarasan.sp@lowes.com>
2023-03-21 23:06:35 +0530

    initial commit

---
+++ app/src/main/java/com/puvi/introtocompose/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.puvi.introtocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.puvi.introtocompose.ui.theme.IntroToComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntroToComposeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val moneyCounter = remember {
+        mutableStateOf(0)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(
+                text = "$${moneyCounter.value}", style = TextStyle(
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 30.sp
+                )
+            )
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle(moneyCounter.value){
+                moneyCounter.value = it+1
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateCircle(value: Int = 0, updateCount: (Int) -> Unit) {
+
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(45.dp)
+            .clickable {
+                updateCount(value)
+            },
+        shape = CircleShape,
+        elevation = 4.dp
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "tap")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    IntroToComposeTheme {
+        MyApp()
+    }
+}
